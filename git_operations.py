102ced7df3abce04f03cee91c25e9d9b730b45b1
Daniel Carpenter <59896063+Daniel-Carpenter@users.noreply.github.com>
2024-09-03 11:23:17 -0500

    Add branching ability

--- git_operations.py
+++ git_operations.py
@@ -1,16 +1,16 @@
 import subprocess
 from datetime import datetime
 
-def git_pull():
+def git_pull(branch='main'):
     try:
-        # Pulling the latest changes from the remote repository
-        subprocess.run(['git', 'pull'], check=True, capture_output=True)
-        print("Successfully pulled updates from remote.")
+        # Pulling the latest changes from the specified remote repository and branch
+        subprocess.run(['git', 'pull', 'origin', branch], check=True, capture_output=True)
+        print("Successfully pulled updates from Git remote.")
     except subprocess.CalledProcessError as e:
-        print("Warning: Pull operation failed. No changes made.")
+        print("Warning: Git pull operation failed. No changes made.")
         print(f"Error details: {e.stderr.decode().strip()}")
 
-def git_commit_push():
+def git_commit_push(branch='main'):
     try:
         # Adding all changes in the git repository
         subprocess.run(['git', 'add', '.'], check=True, capture_output=True)
@@ -19,11 +19,11 @@ def git_commit_push():
         today_date = datetime.now().strftime("%Y-%m-%d")
         
         # Committing the changes with a message
-        commit_message = f"Daily model run {today_date}"
+        commit_message = f"Daily Data Update ({today_date})"
         subprocess.run(['git', 'commit', '-m', commit_message], check=True, capture_output=True)
         
-        # Pushing the changes to the remote repository
-        subprocess.run(['git', 'push'], check=True, capture_output=True)
+        # Pushing the changes to the remote repository and specified branch
+        subprocess.run(['git', 'push', 'origin', branch], check=True, capture_output=True)
         print("Successfully committed and pushed updates.")
     except subprocess.CalledProcessError as e:
         print("Warning: Commit/Push operation failed. No changes made.")
